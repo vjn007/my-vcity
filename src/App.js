705fa26a0c6157cfed816ebac7cb6d789006061d
S VIJAYAN <seelavijayan@gmail.com>
2025-04-18 16:21:19 +0530

    my-vcity-002

--- src/App.js
+++ src/App.js
@@ -4,10 +4,28 @@ const App = () => {
   return (
     <>
       <div className="App">
-        <h1>my vcity</h1>
+        <div className="row">
+          <h1>my vcity</h1>
+          <div className="row">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                  <div className="d-flex justify-content-center bg-primary text-white">
+                    <h1>Buyer</h1>
+                  </div>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                  <div className="d-flex justify-content-center bg-success text-white">
+                    <h1>Seller</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
